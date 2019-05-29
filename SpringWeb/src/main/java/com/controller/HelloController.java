277e06c1d8557442d70de7593c51e0dcde4157f8
chen7696 <sanjisky@qq.com>
2019-05-29 23:34:37 +0800

    update

--- SpringWeb/src/main/java/com/controller/HelloController.java
+++ SpringWeb/src/main/java/com/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
 	@Value("${test}")
 	private String test;
