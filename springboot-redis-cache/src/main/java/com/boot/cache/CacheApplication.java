51ca5258364c309ec0cda32560b85d0a94048c9b
fangwenzheng <fangwenzheng88@163.com>
2019-10-16 17:54:05 +0800

    加入web依赖

--- springboot-redis-cache/src/main/java/com/boot/cache/CacheApplication.java
+++ springboot-redis-cache/src/main/java/com/boot/cache/CacheApplication.java
@@ -3,10 +3,20 @@ package com.boot.cache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableCaching            // 开启Cache 缓存注解
 @SpringBootApplication
+@RestController
 public class CacheApplication {
+
+    @RequestMapping(value="/hello",method= RequestMethod.GET)
+    public String sayHello(){
+        return "hello";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CacheApplication.class,args) ;
     }
